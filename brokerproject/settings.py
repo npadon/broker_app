8e6a267984be6c54c14f7126ad92f52a6ddec878
Nick Padon <npadon@gmail.com>
2018-04-07 12:22:56 -0500

    add static and uninstal django-heroku

--- brokerproject/settings.py
+++ brokerproject/settings.py
@@ -111,9 +111,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
