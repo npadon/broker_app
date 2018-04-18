aad56858ce0687f400b20d5ba76445a4359d5a29
Nick Padon <npadon@gmail.com>
2018-04-18 10:50:34 -0500

    update import

--- broker_app/urls.py
+++ broker_app/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from broker_app import views
+from broker_app.views import index, SurveyCreate, SurveyUpdate, SurveyDelete
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    path('survey/add/', views.SurveyCreate.as_view(), name='survey-add'),
-    path('survey/<int:pk>/', views.SurveyUpdate.as_view(), name='survey-update'),
-    path('survey/<int:pk>/delete/', views.SurveyDelete.as_view(), name='survey-delete'),
+    path('survey/add/', SurveyCreate.as_view(), name='survey-add'),
+    path('survey/<int:pk>/', SurveyUpdate.as_view(), name='survey-update'),
+    path('survey/<int:pk>/delete/', SurveyDelete.as_view(), name='survey-delete'),
 ]
